79d73a7c71ebc6f33e87fd78662e691fc7d42f68
HyungJinHan <han1210_36@naver.com>
2023-03-31 14:42:38 +0900

    가상 디바이스 연동 성공
    콜스택 포화로 스토리보드 에러나는거 잡는 중

--- layout/header/Header.jsx
+++ layout/header/Header.jsx
@@ -4,9 +4,9 @@ import styles from "./header.style";
 
 export default function Header() {
   return (
-    <View style={styles.viewStyle}>
+    <SafeAreaView style={styles.viewStyle}>
       <Text style={styles.headerText}>Header</Text>
-    </View>
+    </SafeAreaView>
 
     // SafeAreaView : 노치 부분에 가려지지 않도록 하기 위함
   );
